ff73ce680ff6366a8646713f70e66b93330fdf0b
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2020-12-10 16:40:51 +0530

    Update and rename Kth Largest Element in an array to Kth Largest Element in an array.py

--- Kth Largest Element in an array.py
+++ Kth Largest Element in an array.py
@@ -1,6 +1,6 @@
-Leetcode link : https://leetcode.com/problems/kth-largest-element-in-an-array/
-
+#Leetcode link : https://leetcode.com/problems/kth-largest-element-in-an-array/
 #Suresh G --> Python 3
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
