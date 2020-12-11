dbdd2306a676514dbb77ff3502550521a41a06bb
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2020-12-11 18:03:20 +0530

    Update and rename Palindrome Number to Palindrome_Number.py

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,5 +1,5 @@
-Leetcode link: https://leetcode.com/problems/palindrome-number/
-# Suresh G -->
+#Leetcode link: https://leetcode.com/problems/palindrome-number/
+# Suresh G --> Python 3
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x)==str(x)[::-1]
