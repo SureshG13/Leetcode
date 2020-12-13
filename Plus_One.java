a623f84d100f3494f151180ae2f2f6c1c132ded9
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2020-12-13 13:22:24 +0530

    Update and rename Plus One to Plus_One.java

--- Plus_One.java
+++ Plus_One.java
@@ -1,5 +1,5 @@
-Leetcode link : https://leetcode.com/problems/plus-one/description/
-#Suresh G --> Java
+//Leetcode link : https://leetcode.com/problems/plus-one/description/
+//Suresh G --> Java
 
 class Solution {
 public int[] plusOne(int[] digits) 
