b80924bbf05d61d19ff670bb02a5ba27a2ce8007
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2020-12-21 11:33:42 +0530

    Create Regukar_Expression_Matching.py

---
+++ Regukar_Expression_Matching.py
@@ -0,0 +1,8 @@
+#Leetcode link: https://leetcode.com/problems/regular-expression-matching/solution/
+# Suresh G --> Python3
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return s.matches(p);
+    }
+}
