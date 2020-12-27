7e2b7670de657771f37334b1db84d0efa5e64c0b
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2020-12-27 12:53:21 +0530

    Create Multiply_Strings.py

---
+++ Multiply_Strings.py
@@ -0,0 +1,7 @@
+# Leetcode link: https://leetcode.com/problems/multiply-strings/
+# Suresh G --> Python3
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1)*int(num2))
+        
