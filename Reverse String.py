bacf98a7e3ac99371cb7867046b93cd43733e6c4
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2021-01-04 15:03:37 +0530

    Create Reverse String.py

---
+++ Reverse String.py
@@ -0,0 +1,10 @@
+# Leetcode link: https://leetcode.com/problems/reverse-string/
+# Suresh G --> Python3
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s=s.reverse()
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
