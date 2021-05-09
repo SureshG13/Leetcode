43da03cc3b0bb9976a54956d7ebdb0281914c411
Suresh Gedela <75598983+SureshG13@users.noreply.github.com>
2021-05-09 20:57:05 +0530

    Create Combine_Two_tables.sql

---
+++ Combine_Two_tables.sql
@@ -0,0 +1,7 @@
+# link: https://leetcode.com/problems/combine-two-tables/
+# Suresh G --> SQL
+
+# Write your MySQL query statement below
+select FirstName, LastName, City, State
+from Person left join Address
+on Person.PersonId = Address.PersonId;
